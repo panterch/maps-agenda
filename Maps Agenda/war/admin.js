cc7bbc53ffa37856b96f888d904116499c38e688
Thomas Bugnon <tobulogic@google.com>
2012-10-26 15:54:00 +0200

    started on admin page

---
+++ Maps Agenda/war/admin.js
@@ -0,0 +1,49 @@
+// Function to attach an event to an element. IE compatible.
+function AttachEvent(element, type, handler) {  
+  if (element.addEventListener) 
+    element.addEventListener(type, handler, false);  
+  else 
+    element.attachEvent("on"+type, handler);  
+}  
+
+// When the page is loaded, make the menu work.
+function onLoadAdmin() {
+  // Set the size of the content area.
+  var main = document.getElementById("main");
+  var menu = document.getElementById("menu");
+  var content = document.getElementById("content");
+  var space = 10;
+  content.style.marginLeft = space;
+  content.style.width = main.offsetWidth - menu.offsetWidth - space;
+
+  // Attach the onClick handlers to the menu items.
+  var elements = document.getElementsByClassName("menu_item");
+  for (var i = 0; i < elements.length; i++) {
+    AttachEvent(elements[i], "click", itemClickHandler);
+  }
+
+  // Select the first menu item.
+  itemClick(elements[0]);
+}
+
+// Function that translates the event to the item that fired it.
+function itemClickHandler(e) {
+  e = e || window.event;  
+  return itemClick(e.target || e.srcElement);
+}
+
+// item of the menu has been clicked. Move the selector.
+function itemClick(item) {
+  var selector = document.getElementById("menu_selector");
+  selector.style.width = item.offsetWidth;
+  selector.style.height = item.offsetHeight;
+  selector.style.top = item.offsetTop;
+  selector.style.left = item.offsetLeft;
+
+  document.getElementById("content").innerHTML = item.id;
+
+  return false;
+}
+
+
+
