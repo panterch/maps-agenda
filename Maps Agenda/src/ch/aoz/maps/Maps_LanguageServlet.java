77e61e5c967bd1d0c5b3077a7c3dadcdf53bc53c
Thomas Bugnon <tobulogic@google.com>
2013-09-25 17:45:15 +0200

    setting up languages

---
+++ Maps Agenda/src/ch/aoz/maps/Maps_LanguageServlet.java
@@ -0,0 +1,33 @@
+package ch.aoz.maps;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.servlet.http.*;
+
+import ch.aoz.maps.Language;
+
+@SuppressWarnings("serial")
+public class Maps_LanguageServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        HashMap<String, Language> languages = Language.getAllLanguages();
+        
+        StringBuilder response = new StringBuilder();
+        response.append("function LanguageMenuCtrl($scope) {\n");
+        response.append("  $scope.languages = [\n");
+        for (Language lang : languages.values()) {
+          response.append("    {label: '");
+          response.append(lang.getName());
+          response.append("', code: '");
+          response.append(lang.getCode());
+          response.append("'},\n");
+        }
+        response.append("  ];\n");
+        response.append("};");
+                
+        resp.setContentType("text/javascript");
+        resp.getWriter().println(response.toString());
+    }
+}
