f15369e4882d34ed5662fcd049fd35d214631a42
tobulogic <tobulogic@gmail.com>
2014-11-23 13:33:59 +0100

    adding phrases to datastore

--- Maps Agenda/src/ch/aoz/maps/Phrases.java
+++ Maps Agenda/src/ch/aoz/maps/Phrases.java
@@ -2,7 +2,10 @@ package ch.aoz.maps;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashSet;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 
 /** 
@@ -14,18 +17,27 @@ public class Phrases {
   private String lang;
   private List<Phrase> phrases;
   private boolean isOk;
+  private String debug;
   
   public Phrases(List<Phrase> phrases) {
     this.phrases = phrases;
     isOk = false;
     if (phrases.isEmpty()) {
+      debug = "empty";
       return;
     }
     lang = phrases.get(0).getLang();
+    HashSet<String> keys = new HashSet<String>();
     for (Phrase phrase : phrases) {
-      if (phrase.getLang() != lang) {
+      if (!phrase.getLang().equals(lang)) {
+        debug = "diff langs" + lang + " != " + phrase.getLang();
         return;
       }
+      if (keys.contains(phrase.getKey())) {
+        debug = "dup keys";
+        return;
+      }
+      keys.add(phrase.getKey());
     }
     isOk = true;
   }
@@ -41,7 +53,46 @@ public class Phrases {
       }
     }
   }
-  
+
+  /**
+   * Returns the Entity representation of this Phrases.
+   *
+   * @return an Entity with the properties of this Phrases.
+   */
+  public Entity toEntity() {
+    Entity phrases = new Entity(entityKind, this.lang);
+    for (Phrase p : this.phrases) {
+      phrases.setProperty(p.getKey(), packPhrase(p));
+    }
+    return phrases;
+  }
+
+  /**
+   * Add this Phrases to the datastore.
+   *
+   * @return true if this operation succeeded.
+   */
+  public boolean addToStore() {
+    if (!this.isOk()) {
+      return false;
+    }
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    try {
+      datastore.put(this.toEntity());
+    } catch (Exception ex) {
+      return false;
+    }
+    return true;
+  }
+ 
+  /** 
+   * Extracts a Phrase from the packed representation in the database.
+   * 
+   * @param lang language the phrase is in
+   * @param key key of the phrase
+   * @param value string of the phrase in the language
+   * @return a fully constructed Phrase
+   */
   public static Phrase extractPhrase(String lang, String key, String value) {
     String[] values = value.split(",", 3);
     if (values.length != 3) return null;
@@ -49,7 +100,31 @@ public class Phrases {
     return new Phrase(key, lang, values[2], values[1], isTag);
   }
   
+  /**
+   * Packs the provided Phrase into its database representation.
+   * 
+   * @param p the phrase to pack in its database representation.
+   * @return the packed representation.
+   */
+  public static String packPhrase(Phrase p) {
+    StringBuilder s = new StringBuilder();
+    s.append(p.isTag() ? "tag," : "notag,");
+    s.append(p.getGroup());
+    s.append(",");
+    s.append(p.getPhrase());
+    return s.toString();
+  }
+  
   public List<Phrase> getPhrases() {
     return phrases;
+  }
+  public String getLang() {
+    return lang;
+  }
+  public boolean isOk() {
+    return isOk;
+  }
+  public String debug() {
+    return debug;
   }
 }
---
+++ Maps Agenda/src/ch/aoz/maps/Tmp_Phrases.java
@@ -0,0 +1,44 @@
+package ch.aoz.maps;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.*;
+
+import ch.aoz.maps.Language;
+import ch.aoz.maps.Phrase;
+
+@SuppressWarnings("serial")
+public class Tmp_Phrases extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+      Map<String, Language> langs = Language.getAllLanguages();
+      Map<String, String> added = new HashMap<String, String>();
+      for (String lang : langs.keySet()) {
+        List<Phrase> laPhrases = Phrase.GetPhrasesForLanguage(lang);
+        Phrases p = new Phrases(laPhrases);
+        if (!p.isOk()) {
+          added.put(lang, p.debug());
+        } else {
+          added.put(lang, Boolean.toString(p.addToStore()));
+        }
+      }
+      answer(resp, added);
+    }
+    
+    public void answer(HttpServletResponse resp, Map<String, String> added) 
+            throws IOException {
+      StringBuilder response = new StringBuilder();
+      for (String key : added.keySet()) {
+        response.append(key);
+        response.append(": ");
+        response.append(added.get(key));
+        response.append("\n");
+      }
+      resp.setContentType("text/plain");
+      resp.getWriter().println(response.toString());      
+    }
+}
