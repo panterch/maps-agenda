211602ede0a4d731ab3c6410a98815e08300a825
Thomas Bugnon <tobulogic@google.com>
2014-11-28 17:14:06 +0100

    unused import

--- Maps Agenda/src/ch/aoz/maps/Maps_LanguageServlet.java
+++ Maps Agenda/src/ch/aoz/maps/Maps_LanguageServlet.java
@@ -3,7 +3,6 @@ package ch.aoz.maps;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.http.*;
